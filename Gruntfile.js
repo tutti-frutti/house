3714c07804bb1efed10f7fbe68dcecb0a94261d4
Anton <anton.verstka@gmail.com>
2016-09-27 10:53:27 +0300

    first commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,14 +5,38 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-postcss");
   grunt.loadNpmTasks("grunt-sass");
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-csso');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-bake');
 
   grunt.initConfig({
     sass: {
       style: {
         files: {
-          "css/style.css": "sass/style.scss"
+          "build/css/style.css": "sass/style.scss"
         }
       }
+    },
+      
+    csso: {
+        style: {
+            options: {
+                report: "gzip"
+            },
+            files: {
+                // основной файл css
+                "build/css/style.min.css": ["build/css/style.css"],
+                // файлы css из плагинов для минификации и объеденения
+                "build/css/library.min.css": [
+                    "js/library-css/timelify.css",
+                    "js/library-css/slicknav.css",
+                    "js/library-css/selectric.css"
+                ]
+            }
+        }
     },
 
     postcss: {
@@ -28,20 +52,17 @@ module.exports = function(grunt) {
             ]})
           ]
         },
-        src: "css/*.css"
+        src: "build/css/*.css"
       }
     },
 
     browserSync: {
       server: {
         bsFiles: {
-          src: [
-            "*.html",
-            "css/*.css"
-          ]
+          src: ["build/*.html", "build/css/*.css", "build/js/*.js"]
         },
         options: {
-          server: ".",
+          server: "build",
           watchTask: true,
           notify: false,
           open: true,
@@ -51,15 +72,106 @@ module.exports = function(grunt) {
     },
 
     watch: {
+        bake: {
+            files: [ "app/**/**" ],
+            tasks: "bake:build"
+        },
+        html: {
+            files: ["*.html"],
+            tasks: ["copy:html"]
+        },
+        
       style: {
         files: ["sass/**/*.{scss,sass}"],
-        tasks: ["sass", "postcss"],
+        tasks: ["sass", "postcss", "csso"],
         options: {
           spawn: false
         }
       }
-    }
+    },
+      
+    copy: {
+        build: {
+            files: [{
+                expand: true,
+                src: [
+                    "fonts/**/*.{woff, woff2}",
+                    "img/**",
+                    "js/**",
+                    "*.html"
+                ],
+                dest: "build"
+            }]
+        },
+        html: {
+            files: [{
+                expand: true,
+                src: ["*.html"],
+                dest: "build"
+            }]
+        }
+    },
+      
+      clean: {
+          build: ["build"],
+      },
+      
+      concat: {
+      options: {
+          // "включает" использование баннера
+          stripBanners: true, 
+          banner: "/**/"
+      },
+          dist: {
+              // файлы для склеивания
+              src: ['js/library-js/jquery.timelify.js',
+                    'js/library-js/jquery.slicknav.min.js', 
+                    'js/library-js/jquery.selectric.js'], 
+              // где будут находиться склеенные файлы
+              dest: 'build/js/project.js'
+          }
+          
+      },
+      // сжатие файлов js
+      uglify: {
+          my_target: {
+              options: {
+                  beautify: true
+              },
+              files: {
+                  //минимфикация в той же папке, где и основной файл 
+                  "build/js/project.min.js": ['build/js/project.js']
+              }
+          }
+      },
+      bake: {
+          build: {
+              options: {
+                  content: "app/content.json",
+                  section: "de"
+              },
+              
+              files: { 
+                  // указываются из каких шаблонов формируются готовые страницы
+                  // из base.html в index.html
+                  'build/index.html': 'app/base.html', 
+              }
+          }
+      }
+      
   });
 
   grunt.registerTask("serve", ["browserSync", "watch"]);
+  grunt.registerTask('default', ['copy']);
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "sass",
+    "postcss",
+    "csso",
+    "concat",
+    "uglify",
+    "bake",
+//    "imagemin"
+  ]);
 };
